b75e571f9ea0f8ff7d156aacb453a21506c8ebf4
ishita27 <201651019@iiitvadodara.ac.in>
2018-10-01 14:22:34 +0530

    app folder

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from blog.models import Post,Comment
+from django.views.generic import (TemplateView,ListView)
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order.by('-published_date'))
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(CreateView):
+    model = Post
