993839e9416f7e78b68722fe53ef59b9465ed5fb
ishita27 <201651019@iiitvadodara.ac.in>
2019-03-16 17:19:20 +0530

    updated

--- blog/models.py
+++ blog/models.py
@@ -1,42 +1,44 @@
 from django.db import models
 from django.utils import timezone
 from django.core.urlresolvers import reverse
-# Create your models here.
+
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    text= models.TextField()
-    create_date = models.DateTimeField(timezone.now())
-    published_date = models.DateTimeField(blank=True,null=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        publish_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(aprroved_comments=True)
+        return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        #return to the post details after posting
         return reverse("post_detail",kwargs={'pk':self.pk})
+
 
     def __str__(self):
         return self.title
 
+
+
 class Comment(models.Model):
-    post = models.ForeignKey('Post.blog',related_names='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
-    approved_comments = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approved_comments= True
+        self.approved_comment = True
         self.save()
 
     def get_absolute_url(self):
-        # return to post list after commenting on a post
-        return reverse('post_list')
+        return reverse("post_list")
 
     def __str__(self):
         return self.text
